362f978b2abd098e0141f8b1112bf267311fb690
Javier Pons <jpescandell@hotmail.com>
2021-03-26 19:43:00 +0200

    refactoring.

--- api.js
+++ api.js
@@ -2,15 +2,17 @@ const express = require("express");
 const router = express.Router();
 const axios = require('axios');
 
+
+
 router.post("/search",( req, res)=>{
     const searchName = req.body.name;
     let data;
     (async () => {
         try{
-            const resp = await axios.get(`http://open-api.myhelsinki.fi/v1/places/?tags_search=${searchName}`, {}).then(resp => {
-            console.log(resp.data);
+            const fetchingData = await axios.get(`http://open-api.myhelsinki.fi/v1/places/?tags_search=${searchName}`, {}).then(resp => {
             
            data = resp.data;
+           
         })
       
         res.send(JSON.stringify(data))
@@ -22,5 +24,7 @@ router.post("/search",( req, res)=>{
 
    
 })
+
+
 
 module.exports = router;
