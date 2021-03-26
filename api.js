e6a5ad0a8da0c229dc8b0d34227ed5f29deea355
Javier Pons <jpescandell@hotmail.com>
2021-03-26 11:30:51 +0200

    fetching data from myHelsinki API

--- api.js
+++ api.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const axios = require('axios');
 
 router.post("/search",( req, res)=>{
     const searchName = req.body.name;
-    console.log(searchName);
-    res.send('ok')
+    let data;
+    (async () => {
+        try{
+            const resp = await axios.get(`http://open-api.myhelsinki.fi/v1/places/?tags_search=${searchName}`, {}).then(resp => {
+            console.log(resp.data);
+            
+           data = resp.data;
+        })
+      
+        res.send(JSON.stringify(data))
+    }catch(err){
+        console.log(err);
+    }
+        
+    })()
+
+   
 })
 
 module.exports = router;
